62341b3c667b1ae664c49c63e18d77e2a555daa5
AKash <aakashchintakindi91@gmail.com>
2025-02-07 10:08:16 +0530

    Creating the const objects

---
+++ Day_1.js
@@ -0,0 +1,23 @@
+const product = {
+    pen: "Black",
+    price: 270,
+    rating : 4.5,
+    isAvailable: true,
+    discount: 0.05
+};
+// Creating the product object with the properties pen, price, rating, isAvailable, and discount.
+
+
+const person = {
+    name: "Roronoa Zoro",
+    age: 21,
+    isPirate: true,
+    isSwordsman: true,
+    followers: 100000,
+    bounty: 320000000,
+    description: "The Pirate Hunter",
+    post: "Vice Captain",
+    
+}
+
+//creating a onepiece character object with the properties name, age, isPirate, isSwordsman, followers, bounty, description, and post.
